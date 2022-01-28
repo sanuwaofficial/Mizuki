8cd88b755015e5ff1f835335a26c68c1041cef10
Sanuwa <90607090+sanuwaofficial@users.noreply.github.com>
2022-01-28 09:06:35 +0530

    simple test

--- plugins/mizukiscrapers.js
+++ plugins/mizukiscrapers.js
@@ -337,11 +337,13 @@ if (config.WORKTYPE == 'private') {
 
     Mizuki.addCommand({pattern: 'video ?(.*)', fromMe: true, desc: Lang.VIDEO_DESC}, (async (message, match) => {
 
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_VIDEO,MessageType.text, {quoted: message.data});    
-    
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_VIDEO,MessageType.text, {quoted: message.data});
+        if (!match[1].includes('youtube')) return await message.client.sendMessage(message.jid,Lang.NEED_VIDEO,MessageType.text, {quoted: message.data});
+    var svid = match[1].replace("shorts/","watch?v=")
+    var s2vid = svid.replace("?feature=share","")
         var VID = '';
         try {
-            if (match[1].includes('watch')) {
+            if (s2vid.includes('watch')) {
                 var tsts = match[1].replace('watch?v=', '')
                 var alal = tsts.split('/')[3]
                 VID = alal
